047c574aee97a430abcf4867d15f7e739a19d8bc
alexandeadp <alexanderdelrosarioperez@gmail.com>
2026-04-21 14:46:12 -0400

    legibilidad

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -6,13 +6,14 @@ import { Register } from './register';
 import { RegistrationSuccess } from './registration-success';
 import { ForgotPassword } from './forgot-password';
 import { ResetPassword } from './reset-password';
+import { NoAuthGuard } from '../../core/guards/no-auth.guard';
 
 export default [
-    { path: 'access', component: Access },
+    { path: 'access', component: Access, canActivate: [NoAuthGuard] },
     { path: 'error', component: Error },
-    { path: 'login', component: Login },
-    { path: 'register', component: Register },
+    { path: 'login', component: Login, canActivate: [NoAuthGuard] },
+    { path: 'register', component: Register, canActivate: [NoAuthGuard] },
     { path: 'registration-success', component: RegistrationSuccess },
-    { path: 'forgot-password', component: ForgotPassword },
-    { path: 'reset-password/:uid/:token', component: ResetPassword }
+    { path: 'forgot-password', component: ForgotPassword, canActivate: [NoAuthGuard] },
+    { path: 'reset-password/:uid/:token', component: ResetPassword, canActivate: [NoAuthGuard] }
 ] as Routes;
